80b20589084033170e70123cfc37439059cb40fe
Simon Zweers <simon.zweers@hva.nl>
2023-02-08 21:18:55 +0100

    README.md | Opdracht 1 FSAexec af met documentatie

--- src/week1/FSAexec.java
+++ src/week1/FSAexec.java
@@ -1,7 +1,13 @@
+/**
+ * Dit programma is voor de FSAexec automaat (week 1 opdracht).
+ * @author Simon Zweers
+ * @date 08-02-2023
+ */
+
 package week1;
 
 /**
- * Zie de DLO practicum opgaven voor instricties.
+ * Zie de DLO practicum opgaven voor instructies.
  * TODO: make it better and understand the working. Add comments and change/add/refactor code.
  * Use flaci to draw FSA/NFA diagram with transition table and add images to your documentation.
  * Use more output to show the working of this app like:
@@ -13,34 +19,44 @@ package week1;
 public class FSAexec {
 
     public static void main(String[] args) {
-        // TODO: fix it and test with 4 extra examples
-        //new FSAexec().fsaExec("abcabaxaaba");
+        System.out.println("Simon Zweers, IT102\n");
+
+
+        new FSAexec().fsaExec("abcabacaaba");
+        new FSAexec().fsaExec("ababacaaba");
         new FSAexec().fsaExec("abcabaaaba");
+        new FSAexec().fsaExec("aabcaabcab");
+        new FSAexec().fsaExec("abcabacaabac");
+        new FSAexec().fsaExec("bcaabbabac");
+        new FSAexec().fsaExec("baabababaabcaabaabac");
     }
 
     /**
-     * TODO: Understand the code and make it better.
-     *
      * @param input the input string for this automaton
      */
-
     void fsaExec(String input) {
-        boolean[] acceptState = {false, false, false, true};
+        // Array met geldige eindstaten
+        boolean[] acceptFinalState = {false, false, false, true};
 
-        int[][] tranTab = { // transition table for 3 different input symbols
+        // Transition table met overgangen voor elke staat (a, b, c)
+        int[][] statesTransitions = { // transition table for 3 different input symbols
                 {0, 1, 0}, // state [0]
                 {2, 1, 1}, // state [1]
                 {0, 1, 3}, // state [2]
                 {0, 1, 3}  // state [3]
         };
-        System.out.println("print the input string");
+        System.out.printf("Input string: %s\n", input);
         int state = 0;
         for (int i = 0; i < input.length(); i++) {
-            char inchar = input.charAt(i);
-            int incharIndex = inchar - 'a';
-            state = tranTab[state][incharIndex];
+            char inputChar = input.charAt(i);
+            int inputCharTransition = inputChar - 'a';
+            System.out.printf("State: %d | transition: %c (%d) | going to: %d\n",
+                    state, inputChar, inputCharTransition, statesTransitions[state][inputCharTransition]
+            );
+            state = statesTransitions[state][inputCharTransition];
         }
-        System.out.println(acceptState[state]);
+        System.out.printf("Final state reached: %b\n", acceptFinalState[state]);
+        System.out.println();
     }
 
 }
--- src/week1/README.md
+++ src/week1/README.md
@@ -1,5 +1,54 @@
+# Week 1
 
-## Dit is de readme voor week 1
+Simon Zweers, IT102
 
-Geef hier antwoord op de vragen van week 1 en leg uit hoe je programma werkt.
+Voor de practicumopgaven van week 1 moesten we een aantal dingen doen:
 
+Een van de opgaven was om een automaat te maken in FLACI voor de FSAexec klasse:
+
+![fsaexec-flaci](../../images/fsaexec-flaci.png)
+
+Om te testen heb ik 4 extra inputs aan de automaat gegeven:  
+`abcabacaaba`  
+`ababacaaba`  
+`abcabaaaba`  
+`aabcaabcab`  
+`abcabacaabac`  
+`bcaabbabac`  
+`baabababaabcaabaabac`  
+
+Deze inputs zijn allemaal in FLACI gesimuleerd om te checken:  
+![](../../images/fsaexec-flaci-input-words.png)  
+Ik heb het programma zo aangepast dat na het uitvoeren van de input in de output wordt weergegeven of de eindstaat bereikt is.
+Aan het begin wordt de input string weergegeven.
+Ook wordt per stap aangegeven welke transitie gebeurt, en welke staat het programma naartoe gaat.
+Twee voorbeelden:  
+```text
+Input string: abcabacaaba
+State: 0 | transition: a (0) | going to: 0
+State: 0 | transition: b (1) | going to: 1
+State: 1 | transition: c (2) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+State: 2 | transition: b (1) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+State: 2 | transition: c (2) | going to: 3
+State: 3 | transition: a (0) | going to: 0
+State: 0 | transition: a (0) | going to: 0
+State: 0 | transition: b (1) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+Final state reached: false
+```
+```text
+Input string: bcaabbabac
+State: 0 | transition: b (1) | going to: 1
+State: 1 | transition: c (2) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+State: 2 | transition: a (0) | going to: 0
+State: 0 | transition: b (1) | going to: 1
+State: 1 | transition: b (1) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+State: 2 | transition: b (1) | going to: 1
+State: 1 | transition: a (0) | going to: 2
+State: 2 | transition: c (2) | going to: 3
+Final state reached: true
+```
