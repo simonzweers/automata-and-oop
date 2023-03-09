80df3c09bcb31fafea9f81a2b381ed890088dc48
Simon Zweers <simon.zweers@hva.nl>
2023-03-09 23:43:07 +0100

    week5 | added Animal, Menu and State classes

---
+++ src/week5/Animal.java
@@ -0,0 +1,15 @@
+package week5;
+
+public class Dier {
+    private String name;
+    private String soort;
+    private int leeftijd;
+
+    Dier(String name) {
+        setName(name);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
